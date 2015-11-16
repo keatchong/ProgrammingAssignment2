5174accd0fa752f7670cdda1ba6dbe640df01a0c
keatchong <keatmin@gmail.com>
2015-11-16 21:43:55 +0800

    Version for peer assessment

--- cachematrix.R
+++ cachematrix.R
@@ -1,29 +1,28 @@
-## the below two functions would enable 
-## 1) creation of a special 'matrix' object that can cache its inverse
-## 2) compute inverse of the matrix and save it to cache or retrieve the inverse 
-## solely from cache if it's available
+## The below two functions would enable 
+## 1) Creation of a special 'matrix' object that can cache its inverse
+## (Matrix supplied is asummed to be always invertible)
+## 2) Compute inverse of the matrix object from 1) and save it to cache or retrieve 
+## the inverse solely from cache if it's available
+## 
 
-
-## this function set the value of the matrix, get the value of thr matrix,
+## this function set the value of the matrix, get the value of the matrix,
 ## set the value for the inverse of the matrix and get the value of the inverse 
 ## matrix. 
 makeCacheMatrix <- function(x = matrix()) {
 
         s <- NULL
         set <- function(y) {
-                        x <<- y
-                        s <<- NULL
-                } 
+                x <<- y
+                s <<- NULL
+        } 
         
         get <- function() x
         setsolve <- function(solve) s <<- solve
         getsolve <- function() s        
         list(set = set, get = get, 
-            setsolve = setsolve,
-            getsolve = getsolve)
-                
+             setsolve = setsolve,
+             getsolve = getsolve)
 }
-
 
 ## this function get the inverse matrix of the 'special' matrix created by 
 ## function makeCacheMatrix. It will checks if the inverse matrix have been 
